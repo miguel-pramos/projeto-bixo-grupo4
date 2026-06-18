e8695c6dfc0610b0ca58e8f58bc4e0bdb6292797
Miguel <miguelpereiraramos1108@gmail.com>
2026-06-18 10:54:32 -0300

    config: preparação da aula 2

---
+++ Eletrônica e Potência/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32-C3!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(10); // this speeds up the simulation
+}
